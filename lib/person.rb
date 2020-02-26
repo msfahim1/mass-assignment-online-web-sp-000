ba1daee27b34b6b9320e95aa6b2fd0ecbda21939
Michael Fahim <michael_fahim@yahoo.com>
2020-02-26 02:24:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,32 @@
 class Person
   #your code here
+  let(:avi_attributes) {
+   {
+     :name => "Avi",
+     :birthday => "01/29/1984",
+     :hair_color => "brown",
+     :eye_color => "brown",
+     :height => "short",
+     :weight => "good",
+     :handed => "lefty",
+     :complexion => "decent",
+     :t_shirt_size => "medium",
+     :wrist_size => "small",
+     :glove_size => "normal",
+     :pant_length => "32",
+     :pant_width => "32"
+   }
+ }
+
+ let(:avi) { Person.new(avi_attributes) }
+
+ let(:spencer_attributes) {
+   {
+     :name => "Spencer",
+     :hair_color => "N/A",
+     :height => "medium",
+     :weight => "good",
+     :handed => "righty",
+   }
+ }
 end
